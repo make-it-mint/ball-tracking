b061672322d901a55bca8ce70a1f99f3ced82e5e
Kevin Fortmann <kevin_fortmann@gmx.de>
2023-06-22 11:48:24 +0200

    changes of ball tracking methods which include functions for the main programm

---
+++ ball_tracking_methods.py
@@ -0,0 +1,183 @@
+# -- coding: utf-8 --
+
+import numpy as np
+import cv2 as cv
+
+def thresholdBest(frame, back_sub, cap):
+    
+    fgMask = back_sub.apply(frame, learningRate = 0.7)
+    
+    # Our operations on the frame come here
+    gray = cv.cvtColor(frame, cv.COLOR_BGR2GRAY)
+
+    # puuting the mask above the original video
+    mask_frame = cv.bitwise_and(gray, gray, mask = fgMask)
+
+    # threshhold for the ball
+    ret, threshold = cv.threshold(mask_frame, 180, 255, cv.THRESH_BINARY)
+    
+    # searching for elipse formed shapes
+    kernel = cv.getStructuringElement(cv.MORPH_ELLIPSE,(9, 9))
+    
+    # mask with imrode / dilate 
+    # kernel = np.ones((5, 5), np.uint8)
+
+    vid_erosion = cv.erode(threshold, kernel, iterations=2)
+    vid_dilation = cv.dilate(vid_erosion, kernel, iterations=3)
+
+    vid_rgb = cv.cvtColor(vid_dilation, cv.COLOR_GRAY2RGB)
+
+    # finding the ball / encircle the ball 
+    # find contours
+    contours, _ = cv.findContours(vid_dilation, cv.RETR_TREE, cv.CHAIN_APPROX_SIMPLE)
+
+    # draw 
+    vid_draw = cv.drawContours(frame, contours, -1, (0, 0, 255), 3)
+
+    # resize
+    scale_percent = 60 # percent of original size
+    width = int(vid_draw.shape[1] * scale_percent / 100) # print out the width and calculate the new width
+    height = int(vid_draw.shape[0] * scale_percent / 100) # print out the height and calculate the new height
+    dim = (width, height)
+
+    resized = cv.resize(vid_draw, dim, interpolation = cv.INTER_AREA)
+
+    cv.rectangle(resized, (10, 2), (100,20), (255,255,255), -1)
+    cv.putText(resized, str(cap.get(cv.CAP_PROP_POS_FRAMES)), (15, 15),
+               cv.FONT_HERSHEY_SIMPLEX, 0.5 , (0,0,0))
+
+    return resized, contours
+
+def kalman_method():
+    kalman_fil = cv.KalmanFilter(4,2)
+    kalman_fil.measurementMatrix = np.array([[1,0,0,0],[0,1,0,0]],np.float32)
+    kalman_fil.transitionMatrix = np.array([[1,0,1,0],[0,1,0,1],[0,0,1,0],[0,0,0,1]],np.float32)
+    kalman_fil.processNoiseCov = np.array([[1,0,0,0],[0,1,0,0],[0,0,1,0],[0,0,0,1]],np.float32) * 0.03
+
+    return kalman_fil
+
+def thresholdChangedOrder(frame, back_sub, cap):
+      
+    # Our operations on the frame come here
+    gray = cv.cvtColor(frame, cv.COLOR_BGR2GRAY)
+    
+    # threshhold for the ball
+    ret, threshold = cv.threshold(gray, 180, 255, cv.THRESH_BINARY)
+
+    fgMask = back_sub.apply(threshold, learningRate = 0.7)
+
+    # puuting the mask above the original video
+    mask_frame = cv.bitwise_and(threshold, threshold, mask = fgMask)
+    
+    # searching for elipse formed shapes
+    kernel = cv.getStructuringElement(cv.MORPH_ELLIPSE,(9, 9))
+    
+    # mask with imrode / dilate 
+    # kernel = np.ones((5, 5), np.uint8)
+
+    vid_erosion = cv.erode(mask_frame, kernel, iterations=2)
+    vid_dilation = cv.dilate(vid_erosion, kernel, iterations=3)
+
+    vid_rgb = cv.cvtColor(vid_dilation, cv.COLOR_GRAY2RGB)
+
+    # finding the ball / encircle the ball 
+    # find contours
+    contours, _ = cv.findContours(vid_dilation, cv.RETR_TREE, cv.CHAIN_APPROX_SIMPLE)
+
+    # draw 
+    vid_draw = cv.drawContours(frame, contours, -1, (0, 0, 255), 3)
+
+    # resize
+    scale_percent = 60 # percent of original size
+    width = int(vid_draw.shape[1] * scale_percent / 100) # print out the width and calculate the new width
+    height = int(vid_draw.shape[0] * scale_percent / 100) # print out the height and calculate the new height
+    dim = (width, height)
+
+    resized = cv.resize(vid_draw, dim, interpolation = cv.INTER_AREA)
+
+    cv.rectangle(resized, (10, 2), (100,20), (255,255,255), -1)
+    cv.putText(resized, str(cap.get(cv.CAP_PROP_POS_FRAMES)), (15, 15),
+               cv.FONT_HERSHEY_SIMPLEX, 0.5 , (0,0,0))
+
+    return resized, contours
+
+def threshold(frame, back_sub, cap):
+    
+    fgMask = back_sub.apply(frame, learningRate = 0.2)
+    
+    # Our operations on the frame come here
+    gray = cv.cvtColor(frame, cv.COLOR_BGR2GRAY)
+
+    # puuting the mask above the original video
+    mask_frame = cv.bitwise_and(gray, gray, mask = fgMask)
+
+    # threshhold for the ball
+    ret, threshold = cv.threshold(mask_frame, 180, 255, cv.THRESH_BINARY)
+    
+    # searching for elipse formed shapes
+    kernel = cv.getStructuringElement(cv.MORPH_ELLIPSE, (13, 13))
+    
+    # mask with imrode / dilate 
+    # kernel = np.ones((5, 5), np.uint8)
+
+    vid_erosion = cv.erode(threshold, kernel, iterations=1)
+    vid_dilation = cv.dilate(vid_erosion, kernel, iterations=3)
+
+    vid_rgb = cv.cvtColor(vid_dilation, cv.COLOR_GRAY2RGB)
+
+    # finding the ball / encircle the ball 
+    # find contours
+    contours, _ = cv.findContours(vid_dilation, cv.RETR_TREE, cv.CHAIN_APPROX_SIMPLE)
+
+    # draw 
+    vid_draw = cv.drawContours(frame, contours, -1, (0, 0, 255), 3)
+
+    # resize
+    scale_percent = 60 # percent of original size
+    width = int(vid_draw.shape[1] * scale_percent / 100) # print out the width and calculate the new width
+    height = int(vid_draw.shape[0] * scale_percent / 100) # print out the height and calculate the new height
+    dim = (width, height)
+
+    resized = cv.resize(vid_draw, dim, interpolation = cv.INTER_AREA)
+
+    cv.rectangle(resized, (10, 2), (100,20), (255,255,255), -1)
+    cv.putText(resized, str(cap.get(cv.CAP_PROP_POS_FRAMES)), (15, 15),
+               cv.FONT_HERSHEY_SIMPLEX, 0.5 , (0,0,0))
+
+    return resized, contours
+
+def matlabDetection(frame, first_frame_imbinarized_inverted, xk, cap):
+
+    ## changing the color of frame into gray tones
+    frame_gray = cv.cvtColor(frame, cv.COLOR_BGR2GRAY)
+    ## imbinarize frame 
+    _, frame_imbinarized = cv.threshold(frame_gray, 180, 255, cv.THRESH_BINARY)
+    ## combinig the imbinarized frame with the inverted one
+    first_frame_combined = cv.bitwise_and(frame_imbinarized, first_frame_imbinarized_inverted) 
+
+    ## searching for elipse formed shapes
+    kernel = cv.getStructuringElement(cv.MORPH_ELLIPSE, (xk, xk))
+
+    vid_erosion = cv.erode(first_frame_combined, kernel, iterations=2)
+    vid_dilation = cv.dilate(vid_erosion, kernel, iterations=3)
+
+    ## finding the ball / encircle the ball 
+    ## find contours
+    contours, _ = cv.findContours(vid_dilation, cv.RETR_TREE, cv.CHAIN_APPROX_SIMPLE)
+
+    ## draw 
+    vid_draw = cv.drawContours(frame, contours, -1, (0, 0, 255), 3)
+
+    ## resize
+    scale_percent = 60 ## percent of original size
+    width = int(vid_draw.shape[1] * scale_percent / 100) ## print out the width and calculate the new width
+    height = int(vid_draw.shape[0] * scale_percent / 100) ## print out the height and calculate the new height
+    dim = (width, height)
+
+    resized = cv.resize(vid_draw, dim, interpolation = cv.INTER_AREA)
+
+    cv.rectangle(resized, (10, 2), (100,20), (255,255,255), -1)
+    cv.putText(resized, str(cap.get(cv.CAP_PROP_POS_FRAMES)), (15, 15),
+               cv.FONT_HERSHEY_SIMPLEX, 0.5 , (0,0,0))
+
+    return resized, contours
--- read_video.py
+++ read_video.py
@@ -2,68 +2,156 @@
 
 import numpy as np
 import cv2 as cv
+import copy
+import pandas as pd
+import ball_tracking_methods
+import time
 
-# video capturing from video file or camera
-# to read a video file insert the file name
-# for a camera insert an integer depending on the camera port
-cap = cv.VideoCapture("Test-Videos/test-game.MP4")
+kernel_size = []
+time_rate = []
+detection_rate = []
 
-fps = cap.get(cv.CAP_PROP_FPS)
-cap.set(cv.CAP_PROP_POS_FRAMES, fps * 57) # start video by sek 57
-print(fps)
+for xk in range (3, 22, 2):
+    ## video capturing from video file or camera
+    ## to read a video file insert the file name
+    ## for a camera insert an integer depending on the camera port
+    cap = cv.VideoCapture("Test-Videos/ball_tracking_test.MP4")
 
-# Video soll in der richtigen Geschwindigkeit abgespielt werden / Wie viele millisekunden braucht ein einzelner Frame (querwert = Zeit in millisekunden)
-frame_time = int(1000/fps)
+    # import csv compare ball tracking data
+    csv = pd.read_csv("X_und_Y_Positionen_des_Balles_Video_ball_tracking_test.csv")
+    print(csv.x_pos[0])
 
-# exit the programm if the camera cannot be oppend, or the video file cannot be read
-if not cap.isOpened():
-    print("Cannot open camera or video file")
-    exit()
+    fps = cap.get(cv.CAP_PROP_FPS)
+    # cap.set(cv.CAP_PROP_POS_FRAMES, fps * 57) # start video by sek 57
+    print(fps)
 
-# background subtraction
-#backSub = cv.createBackgroundSubtractorMOG2()
-backSub = cv.createBackgroundSubtractorKNN()
+    ## Video soll in der richtigen Geschwindigkeit abgespielt werden / Wie viele millisekunden braucht ein einzelner Frame (querwert = Zeit in millisekunden)
+    frame_time = int(1000/fps)
 
-while True:
-    # Capture frame-by-frame
-    ret, frame = cap.read()
+    ## start of manual background subtraction for a better motion detection 
+    ## memorizing the first frame of the video
+    ret, first_frame = cap.read()
+    ## changing the color of the frist frame into gray tones
+    first_frame_gray_vid = cv.cvtColor(first_frame, cv.COLOR_BGR2GRAY)
+    ## changing the color into back and white
+    _, first_frame_imbinarized = cv.threshold(first_frame_gray_vid, 180, 255, cv.THRESH_BINARY)
+    ## imcomplement the first frame 
+    first_frame_imbinarized_inverted = cv.bitwise_not(first_frame_imbinarized)
 
-    # if frame is read correctly ret is True
-    # stop the loop when the frame is not read correctly
-    if not  ret:
-        print("Can't recive frame (stream end?). Exiting ...")
-        break
+    ## exit the programm if the camera cannot be oppend, or the video file cannot be read
+    if not cap.isOpened():
+        print("Cannot open camera or video file")
+        exit()
 
-    fgMask = backSub.apply(frame, learningRate = 0.9)
-    
-    cv.rectangle(frame, (10, 2), (100,20), (255,255,255), -1)
-    cv.putText(frame, str(cap.get(cv.CAP_PROP_POS_FRAMES)), (15, 15),
-               cv.FONT_HERSHEY_SIMPLEX, 0.5 , (0,0,0))
+    ## background subtraction
+    # back_sub = cv.createBackgroundSubtractorMOG2()
+    back_sub = cv.createBackgroundSubtractorKNN()
 
-    # Our operations on the frame come here
-    gray = cv.cvtColor(frame, cv.COLOR_BGR2GRAY)
+    # implementin kalman function
+    kf = ball_tracking_methods.kalman_method()
 
-    # puuting the mask above the original video
-    mask_frame = cv.bitwise_and(gray, gray, mask = fgMask)
+    ball_pos_x = []
+    ball_pos_y = []
+    curr_x = 0
+    curr_y = 0
 
-    # threshhold for the ball
-    ret, threshold = cv.threshold(mask_frame, 180, 255, cv.THRESH_BINARY)
+    # mouse callback function
+    def draw_circle(event,x,y,flags,param):
+        if event == cv.EVENT_LBUTTONDOWN:
+            global frame, curr_x, curr_y
+            frame_copy = copy.deepcopy(frame)
+            cv.circle(frame_copy, (x, y), 20, (255,0,0), 2)
+            global ball_pos_x, ball_pos_y
+            curr_x = x
+            curr_y = y 
+            ## Display the resulting frame
+            # cv.imshow("frame", frame_copy)
 
-    # resize
-    scale_percent = 60 # percent of original size
-    width = int(threshold.shape[1] * scale_percent / 100) # print out the width and calculate the new width
-    height = int(threshold.shape[0] * scale_percent / 100) # print out the height and calculate the new height
-    dim = (width, height)
+    ## clicking the center auf the ball 
+    # cv.namedWindow("frame", cv.WND_PROP_FULLSCREEN)
+    # cv.setMouseCallback("frame",draw_circle)
+    # cv.setWindowProperty("frame",cv.WND_PROP_FULLSCREEN,cv.WINDOW_FULLSCREEN)
 
-    resized = cv.resize(threshold, dim, interpolation = cv.INTER_AREA)
+    frame_count = 0
 
-    # Display the resulting frame
-    cv.imshow("frame", resized)
+    # variabel i for right detection 
+    i = 0
 
-    # stop the loop if the "q" key on the keyboard is pressed 
-    if cv.waitKey(frame_time) == ord("q"):
-        break
+    # stop time - comparing the times how long a algorthm takes to go through the video 
+    start_time = time.time()
 
-# When everything done, release the capture
-cap.release()
-cv.destroyAllWindows()
+    while True:
+        ## Capture frame-by-frame
+        ret, frame = cap.read()
+
+        ## if frame is read correctly ret is True
+        ## stop the loop when the frame is not read correctly
+        if not  ret:
+            print("Can't recive frame (stream end?). Exiting ...")
+            break
+
+        resized, contours = ball_tracking_methods.matlabDetection(frame = frame, 
+                                                        first_frame_imbinarized_inverted = first_frame_imbinarized_inverted,
+                                                        xk = xk, 
+                                                        cap = cap)
+        
+        ## lists for the mid values
+        x_mid = []
+        y_mid = []
+
+
+        for mid_value_contours in contours:
+            # print(mid_value_contours[:, 0][:, 1])
+            
+            x = mid_value_contours[:, 0][:, 0]
+            y = mid_value_contours[:, 0][:, 1]
+
+            x_mid.append(round(np.mean(x)))
+            y_mid.append(round(np.mean(y))) 
+
+            # print(int(np.mean(x)))
+
+        if len(contours) == 1:
+            bp = np.array([[np.float32(x_mid[0])],[np.float32(y_mid[0])]])
+            kf.correct(bp)
+            tp = kf.predict()
+
+        # calculating the falsure percentage 
+        if len(contours) == 1 and abs(x_mid[0] - csv.x_pos[frame_count]) <= 20 and abs(y_mid[0] - csv.y_pos[frame_count]) <= 20:
+            # variabel i for right detection 
+            i += 1
+        
+        frame_count += 1
+        
+
+        # Display the resulting frame
+        cv.imshow("resized", resized)
+
+        # stop the loop if the "q" key on the keyboard is pressed 
+        if cv.waitKey(1) == ord("q"):
+            break
+
+        # ball_pos_x.append(curr_x)
+        # ball_pos_y.append(curr_y)
+
+    # detection of the ball percentage
+    detect_perc = (i * 100) / frame_count
+    print(f"Die Ballerkennungsrate liegt bei {detect_perc}%.")
+
+    # stop time - comparing the times how long a algorthm takes to go through the video 
+    end_time = time.time()
+    elasped_time = end_time - start_time
+    print(f"Die Ausführung des Videos hat {elasped_time}s gedauert.")
+
+    kernel_size.append(xk)
+    time_rate.append(elasped_time)
+    detection_rate.append(detect_perc)
+
+    # When everything done, release the capture
+    cap.release()
+    cv.destroyAllWindows()
+
+## create data frame
+df = pd.DataFrame(data = {"kernel size": kernel_size, "time rate": time_rate, "detection rate": detection_rate})
+## save data frame as cvs data
+df.to_csv("Daten_der unterschiedlichen_Paramter_fuer_die_Ballereknnung_Iteration_2.csv")
